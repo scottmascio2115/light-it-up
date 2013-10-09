f66f20adeae98e376c18e9c545f2d4232b9d84f0
Geoff Crittenden <geoffcritt@hotmail.com>
2013-10-09 17:38:11 -0500

    added routes for login, logout, signup, and user profile and added resources for users, slideshows and slides

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@ LightItUp::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  resources :users, :slideshows, :slides
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
+  get 'signup' => 'users#new'
+  get 'profile/:id' => 'users#show'
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
