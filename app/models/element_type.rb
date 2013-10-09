23f4844e35e5ca8c8608aa16d2ae25eb6707d475
Geoff Crittenden <geoffcritt@hotmail.com>
2013-10-09 16:38:26 -0500

    added models slide, slideshow, element and element_type, dirty lobster

--- app/models/element_type.rb
+++ app/models/element_type.rb
@@ -1,2 +1,6 @@
 class ElementType < ActiveRecord::Base
+
+  has_many :elements
+  has_many :slides, through: :elements
+
 end
--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,2 +1,7 @@
 class Slide < ActiveRecord::Base
+
+  belongs_to :slideshow
+  belongs_to :creator, class_name: 'User'
+  mas_many :elements
+
 end
