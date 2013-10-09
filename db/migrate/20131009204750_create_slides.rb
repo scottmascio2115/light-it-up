f17f451f58bc1d468508a9c449ac6d0e555982cf
Geoff Crittenden <geoffcritt@hotmail.com>
2013-10-09 16:28:42 -0500

    added creator user_id field to slide migration

--- db/migrate/20131009204750_create_slides.rb
+++ db/migrate/20131009204750_create_slides.rb
@@ -2,6 +2,7 @@ class CreateSlides < ActiveRecord::Migration
   def change
     create_table :slides do |t|
       t.integer :slideshow_id
+      t.integer :user_id
       t.string  :title
       t.integer :page
 
