5c661995878d74eeda61530ecd0eef1245a6ffef
Eigil Sagafos <eigil@sagafos.no>
2013-10-09 20:46:55 -0500

    added session helpers and logo image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+
+  def svg(file)
+    file = File.open("app/assets/images/#{file}.svg", "rb")
+    raw file.read
+  end
+
+  def session?
+    session[:user_id] != nil
+  end
+
+  def session_user
+    User.find(session[:user_id]) if session?
+  end
+
 end
