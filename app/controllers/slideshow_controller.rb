3d6781a18c0f0de76d1622f8a80e7a04290edb76
Eigil Sagafos <eigil@sagafos.no>
2013-10-10 11:18:41 -0500

    Created spec files for slide and slideshow and controller files

---
+++ app/controllers/slideshow_controller.rb
@@ -0,0 +1,2 @@
+class SlideController < ApplicationController
+end
---
+++ spec/helpers/slide_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the SlideHelper. For example:
+#
+# describe SlideHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       helper.concat_strings("this","that").should == "this that"
+#     end
+#   end
+# end
+describe SlideHelper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
