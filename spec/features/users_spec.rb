a1dc32f72155ae22befb38b50895f5676ee6c3d6
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-10 19:34:19 -0500

    more user profile intergration tests

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -14,11 +14,19 @@ describe "User Profile" do
   describe "Profile functionality" do
     before(:each) do
       visit user_path(User.find_by_email("ex@fake.com"))
+      save_and_open_page
     end
 
     it "Can get to new slideshow page" do
       click_link 'Create new slideshow'
       page.should have_content 'slideshow'
     end
+
+    it "Can get to new slideshow page" do
+      click_link 'Homepage'
+      page.should have_content 'Welcome to CromeCast Hack'
+    end
+
+
   end
 end
