7b2adc1fc22064175bf4e2942aeef6241ff8c9a6
Geoff Crittenden <geoffcritt@hotmail.com>
2013-10-11 07:55:56 -0500

    added all standard methods to elements_controller.rb

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -1,4 +1,31 @@
 class ElementsController < ApplicationController
 
+  def index
+    
+  end
+
+  def create
+    
+  end
+
+  def new
+    
+  end
+
+  def show
+    
+  end
+
+  def update
+    
+  end
+
+  def edit
+    
+  end
+
+  def destroy
+    
+  end
 
 end
