ab0d379dc21b6cda1186e984df9830693a8c3f63
Eigil Sagafos <eigil@sagafos.no>
2013-10-11 18:01:00 -0500

    Added two element types to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,37 @@
-User.destroy_all
-Slideshow.destroy_all
-Slide.destroy_all
+# User.destroy_all
+# Slideshow.destroy_all
+# Slide.destroy_all
 
-50.times do
-  User.create(email: Faker::Internet.email,
-              password: "password",
-              password_confirmation: "password")
-end
+# 50.times do
+#   User.create(email: Faker::Internet.email,
+#               password: "password",
+#               password_confirmation: "password")
+# end
 
-5.times do
-  user = User.all.sample
-  Slideshow.create(name: Faker::Lorem.sentence(word_count = 3, supplemental = false, random_words_to_add = 3),
-                   shared: [false, true].sample,
-                   user_id: user.id)
-end
+# 5.times do
+#   user = User.all.sample
+#   Slideshow.create(name: Faker::Lorem.sentence(word_count = 3, supplemental = false, random_words_to_add = 3),
+#                    shared: [false, true].sample,
+#                    user_id: user.id)
+# end
 
 
-Slideshow.all.each do |slideshow|
-  rand(3..7).times do
+# Slideshow.all.each do |slideshow|
+#   rand(3..7).times do
 
-    offset = rand(User.count)
+#     offset = rand(User.count)
 
-    if slideshow.shared == false
-      user_id = slideshow.user_id
-    else
-      user_id = User.first(offset: offset).id
-    end
+#     if slideshow.shared == false
+#       user_id = slideshow.user_id
+#     else
+#       user_id = User.first(offset: offset).id
+#     end
 
-    Slide.create(slideshow_id: slideshow.id,
-                 user_id: user_id,
-                 title: Faker::Lorem.sentence(word_count = 3, supplemental = false, random_words_to_add = 10))
-  end
-end
+#     Slide.create(slideshow_id: slideshow.id,
+#                  user_id: user_id,
+#                  title: Faker::Lorem.sentence(word_count = 3, supplemental = false, random_words_to_add = 10))
+#   end
+# end
 
 element_types = [ {name: "Title",
                    before_tag: "<h1>",
@@ -40,3 +40,7 @@ element_types = [ {name: "Title",
                    before_tag: "<img>",
                    after_tag: "</img>"}
                 ]
+
+element_types.each do |type|
+  ElementType.create(type)
+end
