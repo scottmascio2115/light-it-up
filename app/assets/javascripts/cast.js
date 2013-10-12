5403d0e275230d6db56557e67f7f9093d6bad926
Geoff Crittenden <geoffcritt@hotmail.com>
2013-10-12 16:35:50 -0500

    slide advance working

--- app/assets/javascripts/cast.js
+++ app/assets/javascripts/cast.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
-  var $active = $('#slide .active');
+  var $active = $('.active');
   $('.hidden').hide();
  
   $(document).keyup(function(e) {
+    // console.log('before');
     if (e.keyCode == 37) {
-      $active = $('.slide').next();
-      $active.show();
+      console.log('keyup');
+      $active = $('.active').next();
+      $active.prev().fadeOut();
+      $active.fadeIn();
       $active.addClass('active');
       $active.removeClass('hidden');
       $active.prev().removeClass('active');
       $active.prev().addClass('hidden');
-      $active.prev().hide();
+      // $active.prev().hide();
     }
   });
  
